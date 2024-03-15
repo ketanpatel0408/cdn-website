40fcf00737d373438795fa0932fbe1ddc1443ee0
ketanpatel0408 <ketanpatel0408@gmail.com>
2024-03-15 11:53:45 +0530

    changes

--- tractor.js
+++ tractor.js
@@ -10,44 +10,69 @@ $(document).on("change", "#exe_FileUpload", function (e) {
     }
 });
 
-$(document).ready(function () {
-    $("body").append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />');
-    $.getScript('https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', function () {
-        $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js', function () {
-            $.getScript('https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.3/xlsx.full.min.js', function () {
-                var modalHTML = `
-                    <div id="myModal" class="modal fade" data-backdrop="static" data-keyboard="false">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header border-0">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="row mb-5">
-                                        <div class="col-md-12 mb-4">
-                                            <h4>અહીંયા નામ લખીને એક્સેલ અપલોડ કરો</h4>
+function loadOtherScripts() {
+    var bootstrapCSS = document.createElement("link");
+    bootstrapCSS.rel = "stylesheet";
+    bootstrapCSS.href = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css";
+    document.body.appendChild(bootstrapCSS);
+
+    var jqueryScript = document.createElement("script");
+    jqueryScript.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+    jqueryScript.onload = function() {
+        var popperScript = document.createElement("script");
+        popperScript.src = "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js";
+        popperScript.onload = function() {
+            var bootstrapJS = document.createElement("script");
+            bootstrapJS.src = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js";
+            bootstrapJS.onload = function() {
+                var bootstrapBundleJS = document.createElement("script");
+                bootstrapBundleJS.src = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js";
+                bootstrapBundleJS.onload = function() {
+                    var xlsxScript = document.createElement("script");
+                    xlsxScript.src = "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.3/xlsx.full.min.js";
+                    xlsxScript.onload = function() {
+                        var modalHTML = `
+                            <div id="myModal" class="modal fade" data-backdrop="static" data-keyboard="false">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header border-0">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
                                         </div>
-                                        <div class="col-md-12 mb-4">
-                                            <input type='text' id='userName' class="form-control" placeholder="અહીંયા નામ લખો" />                                   
-                                        </div>
-                                        <div class="col-md-12">
-                                            <input type='file' id='exe_FileUpload' />                                   
+                                        <div class="modal-body">
+                                            <div class="row mb-5">
+                                                <div class="col-md-12 mb-4">
+                                                    <h4>અહીંયા નામ લખીને એક્સેલ અપલોડ કરો</h4>
+                                                </div>
+                                                <div class="col-md-12 mb-4">
+                                                    <input type='text' id='userName' class="form-control" placeholder="અહીંયા નામ લખો" />                                   
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <input type='file' id='exe_FileUpload' />                                   
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>`;
-                $("body").append(modalHTML);
-                if ($("#cmbRegCoOp").val() == "2") {
-                    $("#pnlRegMilkDetail").remove();
-                }
-                $("#userName").val($("#txtFName").val());
-                $("#myModal").modal("show");
-            });
-        });
-    });
-});
+                            </div>`;
+                        document.body.insertAdjacentHTML("beforeend", modalHTML);
+                        if (document.querySelector("#cmbRegCoOp").value === "2") {
+                            if($("#pnlRegMilkDetail").length !== 0) {
+                                document.querySelector("#pnlRegMilkDetail").remove();
+                            }
+                        }
+                        document.querySelector("#userName").value = document.querySelector("#txtFName").value;
+                        $('#myModal').modal("show");
+                    };
+                    document.body.appendChild(xlsxScript);
+                };
+                document.body.appendChild(bootstrapBundleJS);
+            };
+            document.body.appendChild(bootstrapJS);
+        };
+        document.body.appendChild(popperScript);
+    };
+    document.body.appendChild(jqueryScript);
+}
 
 
 function excelToPageHandleFile(e) {
